0c76347c33015d7a34dde6bd411b60a29ceada68
Leon Salzmann <leon.salzmann2005@gmail.com>
2023-06-15 10:05:36 +0200

    Fertig gemacht

--- images.py
+++ images.py
@@ -3,30 +3,38 @@ import pygame
 pygame.init() 
 
 player_image = pygame.image.load('world/wiz_r.png')
-player_image = pygame.transform.scale(player_image, (20, 20))
+player_image = pygame.transform.scale(player_image, (16, 16))
 player_image.set_colorkey((0, 0, 0))
 
 player_image_r = pygame.image.load('world/wiz_l.png')
-player_image_r = pygame.transform.scale(player_image_r, (20, 20))
+player_image_r = pygame.transform.scale(player_image_r, (16, 16))
 player_image_r.set_colorkey((0, 0, 0))
 
 player_image_real = player_image
 
-player_image = pygame.transform.scale(player_image, (20, 20))
+player_image = pygame.transform.scale(player_image, (16, 16))
 player_image.set_colorkey((0, 0, 0))
 
 grass_image = pygame.image.load('world/grass.png')
 dirt_image = pygame.image.load('world/dirt.png')
 stone_image = pygame.image.load("world/stone.png")
+stone_dark_image = pygame.image.load("world/stone_dark.png")
 
 shroom_pink_image = pygame.image.load('world/shroom_pink.png')
+shroom_pink_image = pygame.transform.scale(shroom_pink_image, (32, 32))
 shroom_pink_image.set_colorkey((0, 0, 0)) 
 shroom_glow_image = pygame.image.load("world/shroom_glow.png")
+shroom_glow_image = pygame.transform.scale(shroom_glow_image, (32, 32))
 shroom_glow_image.set_colorkey((51,51,51))
 shroom_red_image = pygame.image.load('world/shroom_red.png')
+shroom_red_image = pygame.transform.scale(shroom_red_image, (32, 32))
 shroom_red_image.set_colorkey((0, 0, 0))  
 shroom_green_image = pygame.image.load('world/shroom_green.png')
+shroom_green_image = pygame.transform.scale(shroom_green_image, (32, 32))
 shroom_green_image.set_colorkey((0, 0, 0))
+
+game_over_image = pygame.image.load('world/black.png')
+
 
 GReeeN = (0, 255, 0)
 schwarz = (0, 0 , 0)
@@ -34,3 +42,18 @@ white = (255, 255, 255)
 purple = (255, 0, 255)
 yellow = (255, 255, 0)
 lightblue = (173, 216, 230)
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+level_1_location = [(64 ,96), (156*2, 144*2), (288*2, 208*2), (250*2, 288*2), (40*2, 208*2), (16*2, 144*2), (208*2, 32*2)]
+level_2_location = [(80*2, 16*2), (240*2, 128*2), (214*2, 16*2), (48*2, 80*2), (110*2, 272*2), (280*2, 272*2), (186*2, 192*2)]
+level_3_location = [(80*2, 16*2), (214*2, 16*2), (288*2, 80*2), (208*2, 128*2), (272*2, 16*2), (240*2, 256*2), (62*2, 208*2),(114*2, 240*2), (30*2, 128*2), (114*2, 192*2)]
+level_4_location = [(80*2, 16*2), (214*2, 144*2), (118*2, 112*2), (48*2, 16*2), (32*2, 224*2), (46*2, 288*2), (266*2, 240*2), (256*2, 160*2)]
+
+var = 1
+shroom_location = (32, 640-64)
+
+running = True
+x = True
+uwu = True
+lawnmower = True
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 pygame.display.set_caption("Magic Mushroom")
 
 height = 640
-length = 1120
+length = 640
 FPS = 60
 
 WINDOW_SIZE = (length,height)
@@ -28,6 +28,17 @@ def mapper(datei):
             game_map_t = []
     return game_map 
 
+def check_collision(player_rect, location, radius):
+    # Calculate the distance between the player's center and the location
+    player_center = player_rect.center
+    distance = math.sqrt((player_center[0]*2 - location[0])**2 + (player_center[1]*2 - location[1])**2)
+    
+    # Check if the distance is less than or equal to the radius
+    if distance <= radius:
+        return True
+    else:
+        return False
+
 def blitter(map):
     tile_rects = []
     y = 0
@@ -42,21 +53,12 @@ def blitter(map):
             if tile == '3':
                 display.blit(stone_image, (x * TILE_SIZE, y * TILE_SIZE))
             if tile == '4':
-                #display.blit(shroom_pink_image, (x * TILE_SIZE, y * TILE_SIZE))
+                display.blit(stone_dark_image, (x * TILE_SIZE, y * TILE_SIZE))
                 pass
             if tile == '5':
-                #display.blit(shroom_pink_image, (x * TILE_SIZE, y * TILE_SIZE))
+                #
                 pass
 
-            # Obstacles: -------------------------------------------------------
-            if tile == '6':
-                display.blit(shroom_green_image, (x * TILE_SIZE, y * TILE_SIZE))
-            if tile == '7':
-                display.blit(shroom_red_image, (x * TILE_SIZE, y * TILE_SIZE))
-            if tile == '8':
-                display.blit(shroom_glow_image, (x * TILE_SIZE, y * TILE_SIZE))
-            if tile == '9':
-                display.blit(shroom_pink_image, (x * TILE_SIZE, y * TILE_SIZE))
 
             if tile == '1' or tile == "2" or tile == "3" or tile == "4" or tile == "5":
                 tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
@@ -64,7 +66,7 @@ def blitter(map):
         y += 1
     return tile_rects
 
-game_map = mapper("levels/level_1.txt")
+level1_map = mapper("levels/level_1.txt")
 
 def collision_test(rect, tiles):
     hit_list = []
@@ -100,14 +102,46 @@ moving_left = False
 
 player_y_momentum = 0
 air_timer = 0
+timer = 7*60
+level = 1
+zeit_gesamt = 0
+minuten = 0
 
-player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+player_rect = pygame.Rect(32, 32, player_image.get_width(), player_image.get_height())
 test_rect = pygame.Rect(100,100,100,50)
 
 while True: # game loop
-    display.fill((146,244,255))
+    if zeit_gesamt == 60*60:
+        zeit_gesamt -= 60*60
+        minuten += 1
+    zeit_anzeige = font.render(f"Zeit: {round(timer/60)}", True, "white")
+    zeit_anzeige_rect = zeit_anzeige.get_rect()
 
-    tile_rects = blitter(mapper("levels/level_1.txt"))
+    zeit_ges_anzeige = font.render(f"Zeit: {minuten}min {round(zeit_gesamt/60)}s", True, "white")
+
+    timer -= 1
+    if level <= 4:
+        zeit_gesamt += 1
+    if timer == 0 and level <= 4:
+        level += 1
+        timer = 7*60
+
+    if level == 1:
+        display.fill("black")
+        tile_rects = blitter(mapper("levels/level_1.txt"))
+    elif level == 2:
+        display.fill(lightblue)
+        tile_rects = blitter(mapper("levels/level_2.txt"))
+    elif level == 3:
+        display.fill(lightblue)
+        tile_rects = blitter(mapper("levels/level_3.txt"))
+    elif level == 4:
+        display.fill("black")
+        tile_rects = blitter(mapper("levels/level_4.txt"))
+    elif level > 4:
+        display.fill("black")
+        tile_rects = blitter(mapper("levels/level_end.txt"))
+        pass
 
     player_movement = [0, 0]
     if moving_right:
@@ -153,5 +187,93 @@ while True: # game loop
 
     surf = pygame.transform.scale(display, WINDOW_SIZE)
     screen.blit(surf, (0, 0))
+    if level <= 4:
+        screen.blit(zeit_anzeige, zeit_anzeige_rect)
+    else:
+        screen.blit(game_over_image, (0, 0))
+        screen.blit(surf, (0, 0))
+        screen.blit(zeit_ges_anzeige, (320-100, 320))
+
+    if level == 1:
+        if check_collision(player_rect, shroom_location, 32) == True:
+            shroom_location = level_1_location[random.randint(0,6)]
+            timer += 180
+        screen.blit(shroom_pink_image, shroom_location)
+    
+    if level == 2:
+        if x == True:
+            shroom_location = (80*2, 16*2)
+            x = False
+        if var == 1:
+            screen.blit(shroom_red_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_2_location[random.randint(0,6)]
+                timer += 90
+                var = random.randint(1,2)
+        if var == 2:
+            screen.blit(shroom_green_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_2_location[random.randint(0,6)]
+                timer += 180
+                var = random.randint(1,2)
+
+    if level == 3:
+        if uwu == True:
+            shroom_location = (80*2, 16*2)
+            uwu = False
+        if var == 1:
+            screen.blit(shroom_red_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_3_location[random.randint(0, len(level_3_location)-1)]
+                timer += 60
+                var = random.randint(1,3)
+        if var == 2:
+            screen.blit(shroom_pink_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_3_location[random.randint(0, len(level_3_location)-1)]
+                timer += 180 
+                var = random.randint(1,3)
+        if var == 3:
+            screen.blit(shroom_green_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_3_location[random.randint(0, len(level_3_location)-1)]
+                timer += 100
+                var = random.randint(1,3)
+
+    if level == 4:
+        if lawnmower == True:
+            shroom_location = (80*2, 16*2)
+            lawnmower = False
+        if var == 1:
+            screen.blit(shroom_glow_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_4_location[random.randint(0, 7)]
+                timer += 140
+                var = random.randint(3,4)
+        if var == 2:
+            screen.blit(shroom_red_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_4_location[random.randint(0, 7)]
+                timer += 180
+                var = random.randint(2,4)
+        if var == 3:
+            screen.blit(shroom_green_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_4_location[random.randint(0, 7)]
+                timer += 100
+                var = random.randint(1,2)
+        if var == 4:
+            screen.blit(shroom_pink_image, shroom_location)
+            if check_collision(player_rect, shroom_location, 32) == True:
+                shroom_location = level_4_location[random.randint(0, 7)]
+                timer += 200
+                var = random.randint(1,3)
+
+    if level > 4:
+        while running == True:
+            player_rect.center = (320/2, 320/2)
+            running = False
+        #screen.blit(shroom_pink_image, shroom_location)
+    
     pygame.display.update()
     clock.tick(FPS)
